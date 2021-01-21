ca5255b9ae1d6e6f9ec464c1981867af75021f4e
Meaghan Neylon <mneylon@richmond.edu>
2021-01-20 22:05:19 -0500

    Added data.

---
+++ src/pages/portfolio.data.js
@@ -0,0 +1,47 @@
+const Project_DATA = [
+  {
+    id: 1,
+    title: "Projects",
+    routename: "work",
+    items: [
+      {
+        id: 1,
+        name: "Together Against",
+        concept:
+          "On average, 150 million individuals per year attend professional sporting events. During a global pandemic, spectator attendance deminishes to almost 0. We have developed an app that brings people together and creates an environment to compete against each other.",
+        github: "https://github.com/Bertodemus/LeopardLightning",
+        linked: "https://leopard-ta.herokuapp.com/",
+        imageUrl: require("../components/Images/image01.png"),
+      },
+      {
+        id: 2,
+        name: "Lettuce, Meet Patty",
+        concept:
+          "Perfect place for a burger-fanatic to keep a list of burgers they would like to eat or burgers they have already devoured.",
+        github: "https://github.com/Bertodemus/LeopardLightning",
+        linked: "https://leopard-ta.herokuapp.com/",
+        imageUrl: "../components/Images/image01.png",
+      },
+      {
+        id: 3,
+        name: "Project: PX",
+        concept:
+          "PX is a simple way to have all your social media and news in one central place.",
+        github: "https://github.com/Bertodemus/LeopardLightning",
+        linked: "https://leopard-ta.herokuapp.com/",
+        imageUrl: "image01.png",
+      },
+      {
+        id: 4,
+        name: "Homework: Password Generator",
+        concept:
+          "Password generator that uses Javascript to create a password based on the characters the user would like.",
+        github: "https://github.com/Bertodemus/LeopardLightning",
+        linked: "https://leopard-ta.herokuapp.com/",
+        imageUrl: "../components/Images/image01.png",
+      },
+    ],
+  },
+];
+
+export default Project_DATA;
