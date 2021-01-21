a3e9addf8877392e46adb8e8c2e006a66d3a5370
Meaghan Neylon <mneylon@richmond.edu>
2021-01-20 21:59:51 -0500

    Added Contact component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+function Contact() {
+  return (
+    <div className="contact">
+      <div className="box">
+        <h1> Contact </h1>
+        <div className="form"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
