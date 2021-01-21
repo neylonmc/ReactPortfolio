8b392b10689727f755b909010e0ff364a28a1a02
Meaghan Neylon <mneylon@richmond.edu>
2021-01-20 22:05:58 -0500

    Added state and renderer data.

---
+++ src/pages/portpage.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Project_DATA from "./portfolio.data";
+import Work from "../components/Work";
+
+class ProjectPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      projects: Project_DATA,
+    };
+  }
+
+  render() {
+    const { projects } = this.state;
+    return (
+      <div className="work-page">
+        {projects.map(({ id, ...otherWorkProps }) => (
+          <Work key={id} {...otherWorkProps}></Work>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ProjectPage;
