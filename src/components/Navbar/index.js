433e897b0b8bbe3dee22c5fb24a3d6a5e7709498
Meaghan Neylon <mneylon@richmond.edu>
2021-01-20 22:13:56 -0500

    Updated paths

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import logo from "../Images/logo.png";
+import logo from "../../Images/logo.png";
 
 import "./style.css";
 
