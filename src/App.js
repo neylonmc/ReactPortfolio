17ac95bd57a22ced5e2be5ff82d1f84ff480ca8e
Meaghan Neylon <mneylon@richmond.edu>
2021-01-20 21:55:09 -0500

    Created App.js with Routes to components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import ProjectPage from "./pages/portpage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Route exact path="/" component={About} />
+        <Route exact path="/work" component={ProjectPage} />
+        <Route exact path="/contact" component={Contact} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
