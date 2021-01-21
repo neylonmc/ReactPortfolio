0335d406cd27b00f35a40a6a247cc136313503c3
Meaghan Neylon <mneylon@richmond.edu>
2021-01-20 22:01:33 -0500

    Added footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import background from "../Images/footer.png";
+
+const Footer = () => (
+  <footer
+    class="footer"
+    style={{ backgroundImage: `url(${background})`, height: "100px" }}
+  ></footer>
+);
+
+export default Footer;
