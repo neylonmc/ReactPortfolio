ab2ffa783f3fea1966f0b0c4a8d5ae6a92775f46
Meaghan Neylon <mneylon@richmond.edu>
2021-01-22 12:00:26 -0500

    Updated card to be more responsive

--- src/components/Individual-Projects/index.js
+++ src/components/Individual-Projects/index.js
@@ -1,29 +1,41 @@
 import React from "react";
 import "./style.css";
-import { Card } from "react-bootstrap";
 
 const ProjectItem = ({ name, concept, github, link, imageUrl }) => (
-  <Card style={{ width: "800px" }} className="grid">
-    <img src={imageUrl} alt="pleasework" className="photoImage" />
-    <Card.Body className="card-body">
-      <Card.Title>{name}</Card.Title>
-      <Card.Text>{concept}</Card.Text>
-      <Card.Text>
-        <a href={link}>
-          <i
-            class="fas fa-external-link-alt fa-2x"
-            style={{ color: "black" }}
-          />
-        </a>
-        <a href={github}>
-          <i
-            className="fab fa-github-square fa-2x "
-            style={{ color: "black" }}
-          />
-        </a>
-      </Card.Text>
-    </Card.Body>
-  </Card>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="card-deck">
+        <div class="card">
+          <img class="card-img-top" src={imageUrl} alt="Card image cap" />
+          <div class="card-block">
+            <h4
+              class="card-title"
+              style={{ color: "black", textAlign: "center" }}
+            >
+              {name}
+            </h4>
+            <p class="card-text" style={{ color: "black" }}>
+              {concept}
+            </p>
+            <p class="card-text">
+              <a href={link}>
+                <i
+                  class="fas fa-external-link-alt fa-2x"
+                  style={{ color: "black" }}
+                />
+              </a>
+              <a href={github}>
+                <i
+                  className="fab fa-github-square fa-2x "
+                  style={{ color: "black" }}
+                />
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default ProjectItem;
--- src/pages/portfolio.data.js
+++ src/pages/portfolio.data.js
@@ -10,7 +10,7 @@ const Project_DATA = [
         concept:
           "On average, 150 million individuals per year attend professional sporting events. During a global pandemic, spectator attendance deminishes to almost 0. We have developed an app that brings people together and creates an environment to compete against each other.",
         github: "https://github.com/Bertodemus/LeopardLightning",
-        linked: "https://leopard-ta.herokuapp.com/",
+        link: "https://leopard-ta.herokuapp.com/",
         imageUrl:
           "https://i.postimg.cc/J4XpLj0p/Screen-Shot-2021-01-20-at-11-18-13-AM.png",
       },
@@ -20,7 +20,7 @@ const Project_DATA = [
         concept:
           "Perfect place for a burger-fanatic to keep a list of burgers they would like to eat or burgers they have already devoured.",
         github: "https://github.com/neylonmc/Good-Burger-Creator",
-        linked: "sheltered-beyond-99467.herokuapp.com/",
+        link: "sheltered-beyond-99467.herokuapp.com/",
         imageUrl:
           "https://i.postimg.cc/Hst7XLhp/Screen-Shot-2021-01-21-at-9-54-29-PM.png",
       },
@@ -30,7 +30,7 @@ const Project_DATA = [
         concept:
           "PX is a simple way to have all your social media and news in one central place.",
         github: "https://github.com/tremaynet/group-project-1",
-        linked: "https://tremaynet.github.io/group-project-1/",
+        link: "https://tremaynet.github.io/group-project-1/",
         imageUrl:
           "https://i.postimg.cc/yNkMcfNY/Screen-Shot-2021-01-21-at-9-58-06-PM.png",
       },
@@ -40,7 +40,7 @@ const Project_DATA = [
         concept:
           "Password generator that uses Javascript to create a password based on the characters the user would like.",
         github: "https://github.com/neylonmc/Password-Generator",
-        linked: "https://neylonmc.github.io/Password-Generator/",
+        link: "https://neylonmc.github.io/Password-Generator/",
         imageUrl:
           "https://i.postimg.cc/zf69jnLw/Screen-Shot-2021-01-21-at-9-59-29-PM.png",
       },
@@ -50,7 +50,7 @@ const Project_DATA = [
         concept:
           "A timed code quiz with multiple-choice questions. This app is run in the browser and features dynamically updated HTML and CSS powered by JavaScript code.",
         github: "https://neylonmc.github.io/Javascript-Coding-Quiz/",
-        linked: "https://github.com/neylonmc/Javascript-Coding-Quiz",
+        link: "https://github.com/neylonmc/Javascript-Coding-Quiz",
         imageUrl:
           "https://i.postimg.cc/zvS686xp/Screen-Shot-2021-01-21-at-10-00-11-PM.png",
       },
@@ -60,7 +60,7 @@ const Project_DATA = [
         concept:
           "Fitness Tracker using a Mongo database with a Mongoose schema and handle routes with Express.",
         github: "https://github.com/neylonmc/FitnessTracker-",
-        linked:
+        link:
           "https://secret-mesa-83729.herokuapp.com/?id=5fe0d2daf8b1b60017c3dc1e",
         imageUrl:
           "https://i.postimg.cc/LX4ZxZrX/Screen-Shot-2021-01-21-at-10-02-33-PM.png",
