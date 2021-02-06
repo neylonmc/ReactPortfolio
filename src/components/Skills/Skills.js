f44d074895eeb9853ffb1dba27eab22c42df2fdb
Meaghan Neylon <mneylon@richmond.edu>
2021-02-05 21:34:51 -0500

    Edit font-size

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -5,13 +5,13 @@ function Skills() {
   return (
     <div className="skill-section">
       <h1 id="skills-header"> Skills </h1>
-      <i class="fab fa-html5 fa-2x" />
-      <i class="fab fa-css3-alt fa-2x" />
-      <i class="fab fa-js fa-2x" />
-      <i class="fab fa-react fa-2x" />
-      <i class="fab fa-npm fa-2x" />
-      <i class="fab fa-git fa-2x" />
-      <i class="fab fa-bootstrap fa-2x" />
+      <i class="fab fa-html5" />
+      <i class="fab fa-css3-alt" />
+      <i class="fab fa-js" />
+      <i class="fab fa-react" />
+      <i class="fab fa-npm" />
+      <i class="fab fa-git" />
+      <i class="fab fa-bootstrap" />
     </div>
   );
 }
