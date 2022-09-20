a095e4736841e01505b271ad1e75dd677162dd05
Meaghan Neylon <mneylon@richmond.edu>
2022-09-20 13:44:52 -0400

    Update Contact.js

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -10,9 +10,6 @@ function Contact() {
         <h1> Contact </h1>
         <span> Let's get in touch! </span>
         <div className="form" style={{ flexWrap: "wrap" }}>
-          <div>
-            <img src={phone} alt="phone" className="responsive" />
-          </div>
           <a href="mailto:neylonmc@gmail.com" target="_blank" rel="noreferrer">
             <img src={email} alt="email" className="responsive" />
           </a>
